b4ab5eac09044949a12fa65aa82a31083da1b2db
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-03-05 17:00:09 -0500

    se creo un arra para eliminar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//elemento final array//
-const sports = ["soccer", "baseball"];
-const total = sports.push("football", "swimming");
-console.log(sports)
-console.log(total)
+//eliminar elemento del array//
+const numero = [1, 2, 3, 4, 5]
+console.log("Array original:", numero)
+const ultimoElementoEliminado = numero.pop()
+console.log("Último elemento eliminado:", ultimoElementoEliminado)
+console.log("Array después de usar pop():", numero)
