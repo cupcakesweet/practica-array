407ec447236ead8541926825e26307c16d04c0ef
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-03-05 16:44:17 -0500

    se creo unas funciones dinamica

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-//array de valores unicos//
-const fruta = ["mango", "papaya", "melon"]
-const frutas= new Set(fruta)
-console.log(frutas.has("mango"))
-console.log([...frutas])
+//array de valores dinamicos//
+function generarCursosDinamicamente(numberOfCourses, listOfCourses) {
+    const userCourses = []
+    for (let i = 0; i < numberOfCourses; i++) {
+        const course = {
+            coursename: listOfCourses[Math.floor(Math.random() * listOfCourses.length)],
+            progress: Math.floor(Math.random() * 100)
+        }
+        userCourses.push(course)
+    }
+    return userCourses
+}
+
+const cursosDisponibles = ['Matemáticas', 'Física', 'Historia', 'Biología']
+const cursosUsuario = generarCursosDinamicamente(4, cursosDisponibles)
+console.log(cursosUsuario)
