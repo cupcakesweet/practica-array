7817fd939de3d3fab07da2b7dcf384911e67118c
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-03-05 17:04:13 -0500

    se creo un array de agregar elemento

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-//eliminar elemento del array//
-const numero = [1, 2, 3, 4, 5]
-console.log("Array original:", numero)
-const ultimoElementoEliminado = numero.pop()
-console.log("Último elemento eliminado:", ultimoElementoEliminado)
-console.log("Array después de usar pop():", numero)
+//agregar elemento//
+let numero = [4, 5, 6]
+numero.unshift(1, 2, 3)
+console.log(numero)
