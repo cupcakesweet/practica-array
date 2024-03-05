e438b85dd3e5298fe9ea39403e81403234a06f7e
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-03-05 16:50:08 -0500

    se crearon la array de longitud

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-//array de valores dinamicos//
-function generarCursosDinamicamente(numberOfCourses, listOfCourses) {
-    const userCourses = []
-    for (let i = 0; i < numberOfCourses; i++) {
-        const course = {
-            coursename: listOfCourses[Math.floor(Math.random() * listOfCourses.length)],
-            progress: Math.floor(Math.random() * 100)
-        }
-        userCourses.push(course)
-    }
-    return userCourses
-}
-
-const cursosDisponibles = ['Matemáticas', 'Física', 'Historia', 'Biología']
-const cursosUsuario = generarCursosDinamicamente(4, cursosDisponibles)
-console.log(cursosUsuario)
+//array de longitud del array//
+const numero = [1, 2, 3, 4, 5]
+const longitud = numero.length
+console.log(`La longitud del array es: ${longitud}`)
