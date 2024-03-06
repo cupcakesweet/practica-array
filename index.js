8596d40ab9ed2fd0cca7c8c39d5f98afa36af5d3
alejandro <SENA@ARMAPRCCIFOD637.FormacionSena.RED>
2024-03-06 06:22:22 -0500

    ser creo una subseccion del array

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // Convertir un array en un string usando join()//
-const matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ] 
-  console.log(matrix.join())
-  console.log(matrix.join(";"))
-  
+let numeracion=[1,2,3,4,5,6,7,8,9,0]
+let finalDelnumero = unico.slice(-10,-5)
+console.log(finalDelnumero)
