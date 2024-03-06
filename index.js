971a44e786dfcb7d52d045838f571046dc440d8d
alejandro <SENA@ARMAPRCCIFOD637.FormacionSena.RED>
2024-03-06 06:50:54 -0500

    se agregar multiples elementos

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//  Eliminar una porción del array sin modificar el original usando slice() //
-let divide=[1,2,3,4,5,6,7,8]
-let division = divide.slice(0, 5)
-console.log(division)
+//   Agregar múltiples elementos al principio del array usando unshift()//
+let plus1=[0,1,2,3,4]
+let plus=[5,6,7,8,9]
+plus.unshift(...plus1)
+console.log(plus)
